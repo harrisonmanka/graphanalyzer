5eebb81da58d67ec0bc029e5d21f5772ab1fb033
harrisonmanka <hmanka5@gmail.com>
2023-11-06 00:23:37 -0500

    transitive closure actually works... MAYBE

--- GraphAnalyzer.java
+++ GraphAnalyzer.java
@@ -159,11 +159,12 @@ public class GraphAnalyzer<E> {
     }
 
     public void transitiveClosure(){
+        boolean[][] newMatrix = copyMatrix();
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
                 for(int k = 0; k < size; k++){
                     if(adjMatrix[i][j] && adjMatrix[j][k]){
-                        adjMatrix[i][k] = true;
+                        newMatrix[i][k] = true;
                         String s = "" + getVertexByID(Integer.toString(i)) + "--->"
                                 + getVertexByID(Integer.toString(k));
                         newEdges.add(s);
@@ -171,9 +172,18 @@ public class GraphAnalyzer<E> {
                 }
             }
         }
-        //printMatrix();
+        printNewMatrix(newMatrix);
     }
 
+    public boolean[][] copyMatrix(){
+        boolean[][] newM = new boolean[size][size];
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                newM[i][j] = this.adjMatrix[i][j];
+            }
+        }
+        return newM;
+    }
     public void printMatrix(){
         for(int i = 0; i < adjList.size(); i++){
             if(i == 0){
@@ -186,6 +196,24 @@ public class GraphAnalyzer<E> {
             System.out.print(adjList.get(i).getFirst().getId().toString() + " ");
             for(int j = 0; j < adjMatrix.length; j++){
                 System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println();
+    }
+
+    public void printNewMatrix(boolean[][] newM){
+        for(int i = 0; i < adjList.size(); i++){
+            if(i == 0){
+                System.out.print("\t");
+            }
+            System.out.print(adjList.get(i).getFirst().getId().toString() + "     ");
+        }
+        System.out.println();
+        for(int i = 0; i < newM.length; i++){
+            System.out.print(adjList.get(i).getFirst().getId().toString() + " ");
+            for(int j = 0; j < newM.length; j++){
+                System.out.print(newM[i][j] + " ");
             }
             System.out.println("");
         }
